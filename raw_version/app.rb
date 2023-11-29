5bc1db7b032b24628346b9c08179913c8487b68b
Oussama Seven <sevendevl7@gmail.com>
2023-11-29 12:58:04 +0100

    add olld versions in raw_version folder

--- raw_version/app.rb
+++ raw_version/app.rb
@@ -1,8 +1,5 @@
 require 'pry'
-require_relative 'lib/checkout1.rb'
-require_relative 'lib/rules.rb'
-require_relative 'lib/ceo.rb'
-require_relative 'lib/coo.rb'
+require_relative 'checkout1.rb'
 
 data = [
   {
@@ -27,9 +24,9 @@ basket2 = ['FR1', 'FR1'] # 3.11
 basket3 = [ 'AP1', 'AP1', 'FR1', 'AP1'] # 16.61
 
 price_rules = [
-  Ceo.new(data).perform(basket:),
-  Coo.new(data).perform(basket:)
+  'test'
 ]
+
 co1 = Checkout1.new(price_rules)
 basket.each do |v|
   item = data.find { |h| h[:code] == v }
